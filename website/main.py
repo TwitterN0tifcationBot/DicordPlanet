da579fc76c5ff3990b9e2721970882170698b9c4
TwitterN0tifcationBot <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-03-03 09:17:32 +0100

    Create main.py

---
+++ website/main.py
@@ -0,0 +1,24 @@
+"NAME": "DicordPlanet"
+"STATUS": "Made with DicordPlanet!🪐"
+"STATUS_TYPE": "Custom"
+
+"PAGES" = {
+  "Documents",
+  "Create Bot",
+  "Bot settings"
+  "User Settings",
+  "Libary",
+  "Debug",
+## //////// MANEGEMENT OF BOTS ///////////////
+  "Command_Templates",
+  "Create_Commands",
+  "BOT_STATUS",
+  "Events",
+  "Premuim"
+}
+
+import discord(host)
+    if discord.bot(hosted)
+   bot.status("Status": {STATUS}, "TYPE": {STATUS_TYPE})
+else:
+  bot.status("Status": {CUSTOM}, "Type": {CUSTOM_TYPE})
